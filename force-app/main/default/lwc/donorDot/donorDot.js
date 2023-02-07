90ec2194e955c22680203840783a1383ef00ca11
Daniel Shahin <dan@danshahin.com>
2023-02-07 13:36:06 -0800

    Basic Appointment Browsing (#77)
    * Fix failing tests.
    
    * Remove dead code.
    
    * checkonly deployment against production org
    
    * center scheduler wip
    
    * fix bad layouts
    
    * center scheduler updates
    
    * visual mocking for Center Scheduler
    
    * add unit test coverage for controller
    
    * add date display
    
    * wiring up apex to front end
    
    * update unit tests
    
    * comment out artifact upload to speed up build for now
    
    * wire up refresh button
    
    * add plus button for each row
    
    ---------
    
    Co-authored-by: James Holmes <james@jamesholmes.com>

---
+++ force-app/main/default/lwc/donorDot/donorDot.js
@@ -0,0 +1,53 @@
+import { LightningElement, track, api } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+
+export default class DonorDot extends NavigationMixin(LightningElement)  {
+
+    @track show = false;
+    @api initials;
+    @api icon = 'standard:account';
+    @api donor;
+    @api appointment;
+    donorLink;
+    appointmentLink;
+
+    connectedCallback() {
+        
+        this[NavigationMixin.GenerateUrl]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.appointment.Id,
+                actionName: 'view',
+            },
+        }).then((url) => {
+            this.appointmentLink = url;
+        });
+
+        this[NavigationMixin.GenerateUrl]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.donor.donorId,
+                actionName: 'view',
+            },
+        }).then((url) => {
+            this.donorLink = url;
+        });
+    }
+
+    togglePopover() {
+        this.show = !this.show;
+    }
+
+    hidePopover() {
+        this.show = false;
+    }
+
+    donorClick(){
+        window.open(this.donorLink);
+    }
+
+    appointmentClick(){
+        window.open(this.appointmentLink);
+    }
+ 
+}

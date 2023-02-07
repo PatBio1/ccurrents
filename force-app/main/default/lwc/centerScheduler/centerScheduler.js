a0da9a83892606c846d7b4ae27ff018f7ce3cac3
Dan Shahin <dan@danshahin.com>
2023-02-07 11:28:11 -0800

    wiring up apex to front end

--- force-app/main/default/lwc/centerScheduler/centerScheduler.js
+++ force-app/main/default/lwc/centerScheduler/centerScheduler.js
@@ -1,62 +1,17 @@
 import { LightningElement, track } from 'lwc';
 import getCenters from '@salesforce/apex/CenterScheduleController.getCenters';
+import getAppointments from '@salesforce/apex/CenterScheduleController.getAppointments';
 
 export default class CenterScheduler extends LightningElement {
 
     @track selectedCenterId;
     @track selectedDate
+    @track appointments;
     centers = [];
 
     @track showFilters=false;
     @track show = false;
     @track dateDisabled = true;
-
-    get appointments(){
-        return [
-            {
-                key:1,
-                time: "8:00 AM",
-                available: 3,
-                filled: 2,
-                donors:[
-                    {key:1,initials:'KK', icon: 'custom:custom92'},
-                    {key:2,initials:'JK', icon: 'custom:custom71'},
-                ]
-            },
-            {
-                key:2,
-                time: "8:10 AM",
-                available: 5,
-                filled: 0,
-                donors:[
-
-                ]
-            },
-            {
-                key:3,
-                time: "8:20 AM",
-                available: 0,
-                filled: 5,
-                donors:[
-                    {key:1,initials:'WD', icon: 'custom:custom92'},
-                    {key:2,initials:'ZX', icon: 'custom:custom92'},
-                    {key:3,initials:'WD', icon: 'custom:custom88'},
-                    {key:4,initials:'ZX', icon: 'custom:custom79'},
-                    {key:5,initials:'ZX', icon: 'custom:custom63'},
-                ]
-            },
-            {
-                key:4,
-                time: "8:30 AM",
-                available: 2,
-                filled: 2,
-                donors:[
-                    {key:1,initials:'RR', icon: 'custom:custom90'},
-                    {key:2,initials:'GG', icon: 'custom:custom91'},
-                ]
-            },
-        ];
-    }
 
     showPopover() {
         this.show = !this.show;
@@ -120,16 +75,28 @@ export default class CenterScheduler extends LightningElement {
         });
     }
 
+    fetchAppointments(){
+        getAppointments({
+            centerId: this.selectedCenterId,
+            appointmentDay: this.selectedDate
+        }).then(appointments =>{
+            console.log(appointments);
+            this.appointments = appointments;
+        }).catch(err =>{
+            console.error(err);
+        })
+    }
+
     changeCenter(event){
         this.selectedCenterId = event.detail.value;
-        // alert('changed to ' + this.selectedCenterId);
         this.dateDisabled = false;
     }
 
     changeDate(event){
         this.selectedDate = event.detail.value;
-        alert('changed to ' + this.selectedDate);
-
+        this.fetchAppointments();
     }
+
+    
 
 }
--- force-app/main/default/lwc/donorDot/donorDot.js
+++ force-app/main/default/lwc/donorDot/donorDot.js
@@ -1,10 +1,26 @@
 import { LightningElement, track, api } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class DonorDot extends LightningElement {
+export default class DonorDot extends NavigationMixin(LightningElement)  {
 
     @track show = false;
     @api initials;
     @api icon = 'standard:account';
+    @api donor;
+    donorLink;
+
+    connectedCallback() {
+        // Generate a URL to a User record page
+        this[NavigationMixin.GenerateUrl]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.donor.donorId,
+                actionName: 'view',
+            },
+        }).then((url) => {
+            this.donorLink = url;
+        });
+    }
 
     togglePopover() {
         this.show = !this.show;
@@ -12,6 +28,10 @@ export default class DonorDot extends LightningElement {
 
     hidePopover() {
         this.show = false;
+    }
+
+    donorClick(){
+        window.open(this.donorLink);
     }
  
 }

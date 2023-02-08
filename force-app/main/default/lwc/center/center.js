e528101eaeca8f23c56949abdedac66242a94d0b
James Holmes <james@jamesholmes.com>
2023-02-08 01:28:39 -0500

    Complete issue #28.

--- force-app/main/default/lwc/center/center.js
+++ force-app/main/default/lwc/center/center.js
@@ -1,9 +1,46 @@
-import { LightningElement } from 'lwc';
+import { api, track, LightningElement } from 'lwc';
 import labels from 'c/labelService';
+import getCenter from '@salesforce/apex/CenterController.getCenter';
 
 export default class Center extends LightningElement {
 
     labels = labels;
+    currentCenterId;
+    loading = true;
+    loaded = false;
+    @track center = {};
+
+    @api backLabel;
+
+    @api get centerId() {
+        return this.currentCenterId;
+    }
+    set centerId(value) {
+        this.currentCenterId = value;
+
+        this.loadCenter();
+    }
+
+    loadCenter() {
+        this.loading = true;
+
+        const request = {
+            centerId: this.currentCenterId
+        };
+
+        console.log('getCenter request', JSON.stringify(request));
+
+        getCenter(request).then(response => {
+            console.log('getCenter response', response);
+            this.center = response;
+            this.loaded = true;
+        }).catch((error) => {
+            // TODO - add error handling
+            console.log(error);
+        }).finally(() => {
+            this.loading = false;
+        });
+    }
 
     onBackButtonClick() {
         this.dispatchEvent(new CustomEvent('back'));

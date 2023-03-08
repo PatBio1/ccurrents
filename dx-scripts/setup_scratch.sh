101afe2ea5a8e97d528ae02853ad248df15b3da2
Daniel Shahin <dan@danshahin.com>
2023-03-07 20:45:59 -0800

    add mobile publisher playground apps to scratch org setup (#185)

--- dx-scripts/setup_scratch.sh
+++ dx-scripts/setup_scratch.sh
@@ -27,7 +27,12 @@ sfdx force:config:set defaultusername=$1
 
 #install the marketing cloud connect package (source has dependencies on this package)
 # commenting for now - will add back later when dependencies introduced into source
-sfdx force:package:install --package MarketingCloud -w 10 --noprompt 
+sfdx package install --package MarketingCloud -w 10 --no-prompt 
+
+sfdx package install --package MobilePublisherIOS -w 10 --no-prompt
+
+sfdx package install --package MobilePublisherAndroid -w 10 --no-prompt
+
 
 ## push local code artifacts to scratch org
 sfdx force:source:push

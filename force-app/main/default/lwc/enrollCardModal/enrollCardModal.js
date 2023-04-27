21a0f0617b47444e223f32ab27112f15a0ac1c52
Chris Glesmann <chrisglesmann@gmail.com>
2023-04-27 11:21:44 -0700

    Initial Onbe Create Account Call

--- force-app/main/default/lwc/enrollCardModal/enrollCardModal.js
+++ force-app/main/default/lwc/enrollCardModal/enrollCardModal.js
@@ -1,8 +1,11 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import LightningModal from 'lightning/modal';
 
+import linkPaymentCard from '@salesforce/apex/CenterScheduleController.linkPaymentCard';
+
 export default class EnrollCardModal extends LightningModal {
+    @api donorId;
     cardPackageId;
     
     get isInputInvalid() {
@@ -13,7 +16,20 @@ export default class EnrollCardModal extends LightningModal {
         this.cardPackageId = event.detail.value;
     }
 
-    handleSubmitCardEnrollment() {
+    async handleSubmitCardEnrollment() {
+        try {
+            console.log(this.cardPackageId);
+            await linkPaymentCard({ cardId: this.cardPackageId, donorId: this.donorId });
+        } catch (error) {
+            this.dispatchEvent(new ShowToastEvent({
+                title: 'Error',
+                message: error.body.message,
+                variant: 'error'
+            }));
+
+            return;   
+        }
+
         this.dispatchEvent(new ShowToastEvent({
             title: 'Success',
             message: 'Card enrollment submitted',
